cd146823155cc4eb9d67dd098aaaa55dcc8aaf52
Abdul Gani <abdulgani1452@gmail.com>
2023-07-23 21:42:57 +0530

    new file

---
+++ Table.py
@@ -0,0 +1,10 @@
+# def MulTable(num):
+    for i in range(1, 11):
+        print(num, 'x', i, '=', num*i)
+
+
+# num = (int(input("enter the number: ")))
+MulTable(num)
+
+
+#Your task is to uncomment the commented code.
